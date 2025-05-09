e6cd3715b6a25fd88850958121b041c03c4e0a4a
Luca Foscili <45429703+lucafoscili@users.noreply.github.com>
2025-05-09 20:21:54 +0200

    feat: added text outputs to LF_LoadFileOnce

--- modules/nodes/io.py
+++ modules/nodes/io.py
@@ -1,4 +1,4 @@
-import io
+import fnmatch
 import json
 import os
 import piexif
@@ -40,6 +40,7 @@ class LF_LoadAndEditImages:
     def on_exec(self, **kwargs: dict):
         return ()
 # endregion
+
 # region LF_LoadFileOnce
 class LF_LoadFileOnce:
     @classmethod
@@ -63,6 +64,10 @@ class LF_LoadFileOnce:
                 }),
             },
             "optional": {
+                "filter": (Input.STRING, {
+                    "default": "", 
+                    "tooltip": "This field can be used to filter file names. Supports wildcards (*)."
+                }),
                 "ui_widget": (Input.LF_HISTORY, {
                     "default": {}
                 }),
@@ -74,35 +79,49 @@ class LF_LoadFileOnce:
 
     CATEGORY = CATEGORY
     FUNCTION = FUNCTION
-    OUTPUT_IS_LIST = (False, False, True, True)
-    RETURN_NAMES = ("file", "name", "file_list", "name_list")
-    RETURN_TYPES = ("*", "STRING", "*", "STRING")
+    OUTPUT_IS_LIST = (False, False, False, True, True, True)
+    RETURN_NAMES = ("file_blob", "file_content", "name", "file_blob_list", "file_content_list", "name_list")
+    RETURN_TYPES = ("*", "STRING", "STRING", "*", "STRING", "STRING")
 
     def on_exec(self, **kwargs: dict):
         dir: str = normalize_list_to_value(kwargs.get("dir"))
         subdir: bool = normalize_list_to_value(kwargs.get("subdir"))
         strip_ext: bool = normalize_list_to_value(kwargs.get("strip_ext"))
         enable_history: bool = normalize_list_to_value(kwargs.get("enable_history"))
+        filter: str = normalize_list_to_value(kwargs.get("filter")).strip()
         ui_widget: dict = normalize_json_input(kwargs.get("ui_widget", {}))
 
         nodes: list[dict] = ui_widget.get("nodes", [])
         previous_files: dict = { node['value'] for node in nodes } if nodes else set()
 
-        file, file_name = None, None
+        data, file_name, text = None, None, None
+        
         for root, dirs, filenames in os.walk(dir):
             if not subdir:
                 dirs[:] = []
+        
+            if filter and os.sep in filter and filter not in os.path.join(root, ''):
+                continue
+            
             for filename in filenames:
+                if filter and os.sep not in filter:
+                    if not fnmatch.fnmatch(filename, filter):
+                        continue
+                    
                 file_name_stripped = os.path.splitext(filename)[0] if strip_ext else filename
-
                 if file_name_stripped in previous_files:
                     continue
-
+                
                 file_path = os.path.join(root, filename)
-                with open(file_path, 'rb') as f:
-                    file = f.read()
-                    file_name = file_name_stripped
-
+                with open(file_path, "rb") as f:
+                    data = f.read()
+                try:
+                    text = data.decode("utf-8")
+                except UnicodeDecodeError:
+                    text = ""
+        
+                file_name = file_name_stripped
+        
                 if enable_history:
                     create_history_node(file_name_stripped, nodes)
                 break
@@ -114,7 +133,7 @@ class LF_LoadFileOnce:
             "dataset": dataset,
         })
 
-        return (file, file_name, file, file_name)
+        return (data, text, file_name, [data], [text], [file_name])
 # endregion
 
 # region LF_LoadImages
