288356ac0609931088552e104289daeb1fd20d2b
Luca Foscili <45429703+lucafoscili@users.noreply.github.com>
2025-09-16 10:38:19 +0200

    refactor: Refactor seed generators into separate modules
    Moved LF_SequentialSeedsGenerator to its own file and created a seeds package with an __init__.py for category definition. Renamed seeds.py to urandom_seed_generator.py, keeping only LF_UrandomSeedGenerator in that file. This improves code organization and separation of seed generation logic.

--- modules/nodes/seeds/urandom_seed_generator.py
+++ modules/nodes/seeds/urandom_seed_generator.py
@@ -7,64 +7,9 @@ from datetime import datetime
 
 from server import PromptServer
 
-from ..utils.constants import CATEGORY_PREFIX, EVENT_PREFIX, FUNCTION, Input, INT_MAX
-from ..utils.helpers import create_history_node, normalize_json_input, normalize_list_to_value
-
-CATEGORY = f"{CATEGORY_PREFIX}/Seed generation"
-    
-# region LF_SequentialSeedsGenerator
-class LF_SequentialSeedsGenerator:
-    @classmethod
-    def INPUT_TYPES(self):
-        return {
-            "required": {
-                "seed": (Input.INTEGER, {
-                    "default": 0, 
-                    "max": INT_MAX, 
-                    "tooltip": "Seed value from which the other seeds will be progressively increased."
-                }),
-                "enable_history": (Input.BOOLEAN, {
-                    "default": True, 
-                    "tooltip": "Enables history, saving the random seeds at execution time."
-                }),
-            },
-            "optional": {
-                "ui_widget": (Input.LF_HISTORY, {
-                    "default": {}
-                }),
-            },
-            "hidden": {
-                "node_id": "UNIQUE_ID"
-            }
-        }
-    
-    CATEGORY = CATEGORY
-    FUNCTION = FUNCTION
-    RETURN_NAMES = ("seed",) * 20
-    RETURN_TYPES = ("INT",) * 20
-
-    def on_exec(self, **kwargs: dict):
-        seed: int = normalize_list_to_value(kwargs.get("seed"))
-        enable_history: int = normalize_list_to_value(kwargs.get("enable_history"))
-        ui_widget: dict = normalize_json_input(kwargs.get("ui_widget", {}))
-
-        seeds = [seed + i for i in range(20)] 
-
-        nodes: list[dict] = ui_widget.get("nodes", [])
-        dataset: dict = {
-            "nodes": nodes
-        }
-
-        if enable_history:
-            create_history_node(str(seed), nodes)
-
-        PromptServer.instance.send_sync(f"{EVENT_PREFIX}sequentialseedsgenerator", {
-            "node": kwargs.get("node_id"), 
-            "dataset": dataset,
-        })        
-
-        return seeds
-# endregion
+from . import CATEGORY
+from ...utils.constants import EVENT_PREFIX, FUNCTION, Input
+from ...utils.helpers import normalize_json_input
 
 # region LF_UrandomSeedGenerator
 class LF_UrandomSeedGenerator:
@@ -157,11 +102,12 @@ class LF_UrandomSeedGenerator:
             return float("NaN")
 # endregion
 
+# region Mappings
 NODE_CLASS_MAPPINGS = {
-    "LF_SequentialSeedsGenerator": LF_SequentialSeedsGenerator,
     "LF_UrandomSeedGenerator": LF_UrandomSeedGenerator,
 }
+
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "LF_SequentialSeedsGenerator": "Generate sequential seeds",
     "LF_UrandomSeedGenerator": "Urandom Seed Generator",
 }
+# endregion
